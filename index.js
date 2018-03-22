4c344b05b5f6d889cc8dc1cc2a72313f68108163
Sriramajeyam <yesoreyeram@gmail.com>
2018-03-22 01:02:27 +0000

    public files

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const moment = require('moment');
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Chaarvik.com!' + moment().format('MMMM Do YYYY, h:mm:ss a')))
+app.use(express.static('public'));
+
+app.get('/status', (req, res) => res.send('Hello Chaarvik.com! Time in server :' + moment().format('MMMM Do YYYY, h:mm:ss a')))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
