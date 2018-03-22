282a200f416e6f296056d6e3f288c7a62e9a6dd0
Sriramajeyam <yesoreyeram@gmail.com>
2018-03-22 00:57:17 +0000

    MomentJS Installed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const moment = require('moment');
+
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Chaarvik.com!'))
+app.get('/', (req, res) => res.send('Hello Chaarvik.com!' + moment().format('MMMM Do YYYY, h:mm:ss a')))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
